b8dd0bdac97f8409e5286eab9f4587090fb7080d
sd <sd@163.com>
2017-10-20 21:26:28 +0800

    create new file shandian.java;

---
+++ ShanDian.java
@@ -0,0 +1,39 @@
+package model;
+public class ShanDian {
+    private  int id;
+    private  String name;
+    private  int age;
+    private  String sex;
+    private  String height;
+
+    public  void setId(int id){
+      this.id=id;
+    }
+    public  void setName(String name){
+        this.name=name;
+    }
+    public  void setAge(int age){
+        this.age =age;
+    }
+    public void setSex(String sex){
+        this.sex=sex;
+    }
+    public void setHeight(String height){
+        this.height=height;
+    }
+    public  int  getId(){
+        return id;
+    }
+    public  String  getName(){
+        return name;
+    }
+    public  int  getAge(){
+        return age;
+    }
+    public  String  getSex(){
+        return sex;
+    }
+    public  String  getHeight(){
+        return height;
+    }
+}
